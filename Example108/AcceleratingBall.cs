853829e4cd253d709e754687ecc65f2b8b13876e
Menno <114731270+Buldz@users.noreply.github.com>
2023-03-23 09:23:18 +0100

    Update AcceleratingBall.cs

--- Example108/AcceleratingBall.cs
+++ Example108/AcceleratingBall.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics; // Vector2
 using Raylib_cs; // Color
 
@@ -15,7 +16,7 @@ In this class, we have the properties:
 Methods:
 
 - AddChild(Node child)
-- RemoveChild(Node child, bool keepAlive = false)
+- RemoveChild(Node child, bool keepAlive = false)5
 */
 
 namespace Movement
@@ -23,28 +24,38 @@ namespace Movement
 	class AcceleratingBall : SpriteNode
 	{
 		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
-
+		Vector2 Velocity;
+		Vector2 Acceleration;
+		float MaxSpeed = 1000f;
 
 		// constructor + call base constructor
 		public AcceleratingBall() : base("resources/ball.png")
 		{
+			Velocity = new Vector2();
+			Acceleration = new Vector2(40, 30);
 			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 4);
-			Color = Color.RED;
+			
+			Color = Color.SKYBLUE;
 		}
 
 		// Update is called every frame
 		public override void Update(float deltaTime)
 		{
+			Acceleration = new Vector2(40, 30);
+			Velocity = Limit(Velocity, MaxSpeed);
 			Move(deltaTime);
 			WrapEdges();
+			Console.WriteLine(Velocity.Length());
 		}
 
 		// your own private methods
 		private void Move(float deltaTime)
 		{
 			// TODO implement
-			// Position += Velocity * deltaTime;
+			Velocity += Acceleration * deltaTime;
+			Position += Velocity * deltaTime;
 
+			Acceleration *= 0.0f;
 			// accelerate your ball (40, 30) every frame
 			// limit to a maximum speed of 1000 pixels/second
 		}
@@ -59,8 +70,24 @@ namespace Movement
 			// TODO implement...
 			if (Position.X > scr_width)
 			{
-				// ...
+				Position.X = 0;
 			}
+
+			if (Position.Y > scr_height)
+			{
+				Position.Y = 0;
+			}
+		}
+
+		private Vector2 Limit(Vector2 vec2, float max)
+		{
+			Vector2 limited = vec2;
+			if (limited.Length() > max)
+			{
+				limited = Vector2.Normalize(limited);
+				limited *= max;
+			}
+			return limited;
 		}
 
 	}
